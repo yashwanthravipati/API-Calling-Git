502fc3b648e9268dceb6f56bed38a1359b097d65
Yashwanth Ravipati <yashwanth.ravipati@icloud.com>
2021-08-02 08:52:10 -0500

    Initial Commit

---
+++ API Calling Git/API_Calling_GitApp.swift
@@ -0,0 +1,17 @@
+//
+//  API_Calling_GitApp.swift
+//  API Calling Git
+//
+//  Created by Yashwanth Ravipati on 8/2/21.
+//
+
+import SwiftUI
+
+@main
+struct API_Calling_GitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
